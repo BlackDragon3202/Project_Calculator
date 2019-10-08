6c1e294684dc88a3f4084b891d58fe0d70eec73a
Mark <menshih_mark@mail.ru>
2019-10-08 17:50:35 +0300

    Fix

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,7 +5,8 @@ namespace Lab1
     public class Program
     {
 
-        static public bool Check(string str) {
+        static public bool Check(string str)
+        {
             //string str = Console.ReadLine();
             Double num;
             bool isNum = Double.TryParse(str, out num);
@@ -18,75 +19,78 @@ namespace Lab1
             string state = "";
             string str;
             Queue<Double> queue = new Queue<Double>();
-    
-            while (flag) {
-            Console.WriteLine("Hello, do you want to choose option?");
-            string option = Console.ReadLine();
-            if (option.Equals("Yes")) {
-                state = Console.ReadLine();
-            }
-            switch(state) {
-                case "Add":
-                    while (true) {
-                        Console.WriteLine("Enter value");
-                        str = Console.ReadLine();
-                        if (Program.Check(str)) {
-                            queue.Enqueue(Double.Parse(str));
-                        } 
-                        Console.WriteLine("Do you want to stop?");
-                        str = Console.ReadLine();
-                        if (str.Equals("Yes") ) {
-                            break;
-                        }
-                    }
-                    break;
-                case "Single":
-                    SingleOp op = new SingleOp();
-                   
-                    Console.WriteLine(currentVar);
-                    currentVar = op.Action(currentVar);
-                    
-                    break;
 
-                case "Binary":
-                    BinaryOp binOp = new BinaryOp();
-               //     Console.WriteLine("Enter a second variable");
-                 //   str = Console.ReadLine();
-                    Double secondVar;
-                    //if (Program.Check(str)) {
-                    //    secondVar = Double.Parse(str);
-                  //  } else {
-                     //   return;
-                   // }       
-                    secondVar = queue.Dequeue();
-                    currentVar = binOp.Action(currentVar, secondVar);
-                    break;
-               
-                case "Exit":
-                    flag = false;
-                    break;
-                    
-                case "Clear":
-                    currentVar = 0; 
-                    break;
-                
-                case "Change":
-                    bool numb = true;
-                    Console.WriteLine("Enter value");
-                    while (numb) {
-                        str = Console.ReadLine();
-                        if (Program.Check(str)) {
-                            currentVar = Double.Parse(str);
-                            numb = false;
-                        } else {
-                            Console.WriteLine("Incorrect value");
-                        }        
-                    }
-                    break;
-            }
-            Console.WriteLine(currentVar);
+            while (flag)
+            {
+                Console.WriteLine("Hello, do you want to choose option?");
+                string option = Console.ReadLine();
+                if (option.Equals("Yes"))
+                {
+                    state = Console.ReadLine();
+                }
+                switch (state)
+                {
+                    case "Add":
+                        while (true)
+                        {
+                            Console.WriteLine("Enter value");
+                            str = Console.ReadLine();
+                            if (Program.Check(str))
+                            {
+                                queue.Enqueue(Double.Parse(str));
+                            }
+                            Console.WriteLine("Do you want to stop?");
+                            str = Console.ReadLine();
+                            if (str.Equals("Yes"))
+                            {
+                                break;
+                            }
+                        }
+                        break;
+                    case "Single":
+                        SingleOp op = new SingleOp();
+
+                        Console.WriteLine(currentVar);
+                        currentVar = op.Action(currentVar);
+
+                        break;
+
+                    case "Binary":
+                        BinaryOp binOp = new BinaryOp();
+                        Double secondVar;    
+                        secondVar = queue.Dequeue();
+                        currentVar = binOp.Action(currentVar, secondVar);
+                        break;
+
+                    case "Exit":
+                        flag = false;
+                        break;
+
+                    case "Clear":
+                        currentVar = 0;
+                        break;
+
+                    case "Change":
+                        bool numb = true;
+                        Console.WriteLine("Enter value");
+                        while (numb)
+                        {
+                            str = Console.ReadLine();
+                            if (Program.Check(str))
+                            {
+                                currentVar = Double.Parse(str);
+                                numb = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Incorrect value");
+                            }
+                        }
+                        break;
+                }
+                Console.WriteLine(currentVar);
             }
         }
- 
+
     }
 }
